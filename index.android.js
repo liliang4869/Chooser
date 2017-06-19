b99bbcf156a52fe807855c71b7e88231cd9e39f4
liliang4869 <liliang4869@163.com>
2017-06-19 14:17:40 +0800

    20170619

---
+++ index.android.js
@@ -0,0 +1,44 @@
+
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,Dimensions
+} from 'react-native';
+var dataList=['1','2','3','4','5','6']
+var {height,width}=Dimensions.get('window');
+import Chooser from './src/Chooser'
+export default class ChooserDemo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+       <Chooser dataItemStyle={{height:height*0.1,width:width,backgroundColor:'#852369'}}>
+         {dataList.map((data,index)=> <View style={{justifyContent:'center',width:width*0.8,height:height*0.1,alignItems:'center'}} key={index}><Text style={{fontSize:20}}>{data}</Text></View>)}
+       </Chooser>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ChooserDemo', () => ChooserDemo);
---
+++ src/Chooser.js
@@ -0,0 +1,69 @@
+import React,{Component}from 'react'
+import {
+    View,Text,Image,ScrollView,Dimensions,TouchableOpacity
+} from 'react-native'
+var {height,width}=Dimensions.get('window');
+var selected=0;
+var checkedImg;
+var unCheckedImg
+var itemSelectedAction;
+export default class Chooser extends Component{
+    componentWillMount(){
+        this.itemList=this.getChildren();
+        selected=this.props.defaultSelected;
+        checkedImg=this.props.checkedImg;
+        unCheckedImg=this.props.unCheckedImg;
+        itemSelectedAction=(index)=>{this.props.itemSelected(index);this.setState({});}
+    }
+    render(){
+        return (<ScrollView style={[{backgroundColor:'white'},this.props.style]}>
+            {this.itemList.map((data,index)=>{
+                return(<ChooserItem index={index} key={index} data={data} style={this.props.dataItemStyle} rightIconStyle={this.props.checkIconStyle}/>)
+            })}
+            </ScrollView>)
+    }
+
+    getChildren(){
+        let res= new Array(0)
+        let {children}=this.props;
+        if(children.length==undefined){
+            res.push(children)
+        }
+        else {
+            for (let i=0;i<children.length;i++){
+            res.push(children[i])
+            }
+        }
+        return res;
+    }
+}
+Chooser.defaultProps={
+    style:{width:width,height:height},
+    defaultSelected:0,
+    unCheckedImg:<Image style={{height:height*0.1,width:width*0.1}} resizeMode='contain' source={require('./img/unChecked.png')}/>,
+    checkedImg:<Image  style={{height:height*0.1,width:width*0.1}} resizeMode='contain' source ={require('./img/checked.png')}/>,
+    checkIconStyle:{height:height*0.1,flex:1,justifyContent:'center',alignItems:'center'},
+    dataItemStyle:{height:height*0.1,width:width,alignItems:'center'}, 
+    itemSelected:(index)=>{console.log('selected'+index)}
+}
+
+export class ChooserItem extends Component{
+    componentWillMount(){
+        this.index=this.props.index;
+        this.state={
+            selected:selected==this.props.index
+        }
+    }
+
+render(){
+    return (<TouchableOpacity style={[this.props.style,{flexDirection:'row'}]} onPress={()=>{selected=this.index;itemSelectedAction(this.index)}}>
+        {this.props.data}
+        <View style={this.props.rightIconStyle}>
+        {
+            selected==this.index?checkedImg:unCheckedImg
+        }
+        </View>
+    
+    </TouchableOpacity>)
+}
+}
